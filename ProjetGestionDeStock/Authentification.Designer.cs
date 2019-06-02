1168b9a5de3ca015a49e3caf971bbc92e1d63c91
hamza zdaik <night23_7@hotmail.com>
2019-06-02 17:13:43 +0000

    yes

---
+++ ProjetGestionDeStock/Authentification.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ProjetGestionDeStock
+{
+    partial class Authentification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bunifuElipse1 = new ns1.BunifuElipse(this.components);
+            this.Header = new System.Windows.Forms.Panel();
+            this.Main = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Main.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // Header
+            // 
+            this.Header.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(90)))), ((int)(((byte)(49)))));
+            this.Header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Header.Location = new System.Drawing.Point(0, 0);
+            this.Header.Margin = new System.Windows.Forms.Padding(4);
+            this.Header.Name = "Header";
+            this.Header.Size = new System.Drawing.Size(1067, 34);
+            this.Header.TabIndex = 2;
+            // 
+            // Main
+            // 
+            this.Main.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(70)))));
+            this.Main.Controls.Add(this.pictureBox1);
+            this.Main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Main.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Main.Location = new System.Drawing.Point(0, 34);
+            this.Main.Margin = new System.Windows.Forms.Padding(4);
+            this.Main.Name = "Main";
+            this.Main.Size = new System.Drawing.Size(1067, 554);
+            this.Main.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1067, 554);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Authentification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 588);
+            this.Controls.Add(this.Main);
+            this.Controls.Add(this.Header);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Authentification";
+            this.Text = "Authentification";
+            this.Load += new System.EventHandler(this.Authentification_Load);
+            this.Main.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ns1.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.Panel Header;
+        private System.Windows.Forms.Panel Main;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ ProjetGestionDeStock/Authentification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetGestionDeStock
+{
+    public partial class Authentification : Form
+    {
+        public Authentification()
+        {
+            InitializeComponent();
+        }
+
+        private void Authentification_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
